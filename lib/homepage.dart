f13b197677e557d1d73aade9d8e4478bc3526156
Adeyeye Adedamola <damscozyblog@gmail.com>
2023-08-03 22:01:03 +0100

    updated sound to play after image detection

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,7 @@
 
 import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
@@ -9,7 +10,6 @@ import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
-// import 'package:tflite/tflite.dart';
 
 enum Artifacts {
   five('Five naira'),
@@ -37,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   File? _image;
   List? _outputs;
 
+  final player = AudioPlayer();
   final _imagePicker = ImagePicker();
 
   @override
@@ -79,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _isLoading = true;
       _image = File(image.path);
     });
-    await classifyImage(_image);
+    await classifyImage(_image).then((value) => playSound());
   }
 
   Future takePicture() async {
@@ -89,13 +90,12 @@ class _MyHomePageState extends State<MyHomePage> {
       _isLoading = true;
       _image = File(image.path);
     });
-    await classifyImage(_image);
+    await classifyImage(_image).then((value) => playSound());
   }
 
   // Classifying Selected Image
   classifyImage(File? image) async {
     var output = await Tflite.runModelOnImage(
-      // var output = await Tflite.runModelOnImage(
       path: image!.path,
       numResults: 5,
       threshold: 0.5,
@@ -107,6 +107,42 @@ class _MyHomePageState extends State<MyHomePage> {
       // Declear List _outputs in the class will be used to show the classifed class name and confidence
       _outputs = output;
     });
+  }
+
+  playSound() async {
+    if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.five.title)) {
+      await player.play(AssetSource('audios/five_naira.wav'));
+    } else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.ten.title)) {
+      await player.play(AssetSource('audios/ten_naira.wav'));
+    } else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.twenty.title))
+      await player.play(AssetSource('audios/twenty_naira.wav'));
+    else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.fifty.title)) {
+      await player.play(AssetSource('audios/fifty_naira.wav'));
+    } else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.onehundred.title)) {
+      await player.play(AssetSource('audios/hundred_naira.wav'));
+    } else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.twohundred.title)) {
+      await player.play(AssetSource('audios/two_hundred_naira.wav'));
+    } else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.fivehundred.title)) {
+      await player.play(AssetSource('audios/five_hundred_naira.wav'));
+    } else if (_outputs != null &&
+        _outputs!.isNotEmpty &&
+        _outputs![0]["label"].contains(Artifacts.onethousand.title)) {
+      await player.play(AssetSource('audios/one_thousand_naira.wav'));
+    }
   }
 
   @override
@@ -341,51 +377,94 @@ class _MyHomePageState extends State<MyHomePage> {
                               : Container(
                                   child: Text(''),
                                 ),
-                    const Gap(10),
-                    InkWell(
-                      onTap: () {
-                        print('Take Picture Button pressed ...');
-                        takePicture();
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          border: Border.all(
-                            width: 1,
-                            color: Color(0xFF347F47),
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.camera,
-                              size: 20,
-                              color: Color(0xFF347F47),
-                            ),
-                            const Gap(5),
-                            Text(
-                              'Take Picture',
-                              style: GoogleFonts.poppins(
+                    const Gap(20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            print('Take Picture Button pressed ...');
+                            takePicture();
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(
+                                width: 1,
                                 color: Color(0xFF347F47),
-                                fontSize: 16,
                               ),
+                              borderRadius: BorderRadius.circular(8),
                             ),
-                          ],
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.camera,
+                                  size: 20,
+                                  color: Color(0xFF347F47),
+                                ),
+                                const Gap(5),
+                                Text(
+                                  _image == null
+                                      ? 'Take Picture'
+                                      : 'Retake Picture',
+                                  style: GoogleFonts.poppins(
+                                    color: Color(0xFF347F47),
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
+                        // const Gap(20),
+                        InkWell(
+                          onTap: () {
+                            print('Pick Image Button pressed ...');
+                            pickImage();
+                            // _image == null;
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF347F47),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.photo,
+                                  size: 20,
+                                  color: Colors.white,
+                                ),
+                                const Gap(5),
+                                Text(
+                                  _image == null
+                                      ? 'Upload Image'
+                                      : 'Re-upload Image',
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     const Gap(20),
                     InkWell(
-                      onTap: () {
-                        print('Button pressed ...');
-                        pickImage();
-                        // _image == null;
+                      onTap: () async {
+                        print('Sound Button pressed ...');
+                        playSound();
                       },
                       child: Container(
+                        // width: MediaQuery.of(context).size.width * .4,
                         padding: const EdgeInsets.symmetric(
                             horizontal: 20, vertical: 10),
                         decoration: BoxDecoration(
@@ -396,13 +475,13 @@ class _MyHomePageState extends State<MyHomePage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Icon(
-                              Icons.photo,
+                              Icons.audiotrack,
                               size: 20,
                               color: Colors.white,
                             ),
                             const Gap(5),
                             Text(
-                              'Upload Image',
+                              'Play Sound',
                               style: GoogleFonts.poppins(
                                 color: Colors.white,
                                 fontSize: 16,
